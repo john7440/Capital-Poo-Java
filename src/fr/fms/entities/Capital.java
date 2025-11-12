8f516dfcf2fa9873f9de770b1e1c1d7228d81449
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-12 15:23:17 +0100

    ajout d'un constructeur + getter et setter

--- src/fr/fms/entities/Capital.java
+++ src/fr/fms/entities/Capital.java
@@ -1,5 +1,26 @@
 package fr.fms.entities;
 
-public class Capital {
+public class Capital extends City{
+	
+	private String monument;
+
+	// --- constructors----
+	public Capital(String country, String cityName, int numberOfCitizen, String monument) {
+		super(country, cityName, numberOfCitizen);
+		this.setMonument(monument);
+		
+	}
+	
+	
+	//--- getter and setter----
+	public String getMonument() {
+		return monument;
+	}
+	
+	public void setMonument(String monument) {
+		this.monument = monument;
+	}
+
+	
 
 }
