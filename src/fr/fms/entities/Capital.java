7e3037599ab141ba02e67728ff89c23d1bbf5710
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-13 11:11:24 +0100

    ajout de la javadoc

--- src/fr/fms/entities/Capital.java
+++ src/fr/fms/entities/Capital.java
@@ -1,10 +1,24 @@
 package fr.fms.entities;
 
+/**
+ * Represents a capital city, which is a specialized type of City
+ * With an additional attribute for a notable monument
+ */
 public class Capital extends City{
 	
+	/** A monument located in the city */
 	private String monument;
 
 	// --- constructors----
+	
+	 /**
+     * Constructs a Capital object with country, city name, population, and monument
+     *
+     * @param country         The country where the city is located
+     * @param cityName        The name of the city
+     * @param numberOfCitizen The population of the city
+     * @param monument        A monument in the city
+     */
 	public Capital(String country, String cityName, int numberOfCitizen, String monument) {
 		super(country, cityName, numberOfCitizen);
 		this.setMonument(monument);
@@ -12,15 +26,33 @@ public class Capital extends City{
 	}
 	
 	//--- getter and setter----
+	
+	/**
+     * Returns the name of the monument in the city
+     *
+     * @return monument The monument name
+     */
 	public String getMonument() {
 		return monument;
 	}
+	
+	/**
+     * Sets the name of the monument in the city
+     *
+     * @param monument The monument name to set
+     */
 	
 	public void setMonument(String monument) {
 		this.monument = monument;
 	}
 
 	// ----methods----
+	
+	/**
+     * Returns a string representation of the Capital object, including city details and monument
+     *
+     * @return A formatted string with city information
+     */
 	
 	@Override
 	public String toString() {
