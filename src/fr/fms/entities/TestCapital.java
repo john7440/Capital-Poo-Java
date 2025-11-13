9c0f077def02e909c7283d779fc812c31b048167
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-13 09:12:51 +0100

    ajout de test pour la classe Commercial

--- src/fr/fms/entities/TestCapital.java
+++ src/fr/fms/entities/TestCapital.java
@@ -10,14 +10,20 @@ public class TestCapital {
 		
 		System.out.println(paris);
 		System.out.println(londres);
-		System.out.println("---------------------");
+		System.out.println("---------------------\n");
 		
+		//Test Employee
 		Employee bill = new Employee("Bill", "Gates", 65, "Cupertino", new Capital("USA", "Washington", 7600000, "White-House"), "Microsoft", 1500000.0);
 		Employee elon = new Employee("Elon", "Musk", 12, "Palo alto", new City("Afrique du Sud", "Pretoria", 800000), "Tesla", 1500.00);
 		
 		System.out.println(bill);
 		System.out.println(elon);
-
+		System.out.println("---------------------\n");
+		
+		// Test Commercial
+		Commercial dupont = new Commercial("Robert", "Dupont", 50, "Rue des rosiers", new City("France", "Limoges", 133000), "FMS", 120000, (float) 5.0);
+		
+		System.out.println(dupont);
 	}
 
 }
